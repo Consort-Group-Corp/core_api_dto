55dc742497f2445d79b54e087d1aebb89e0e6606
Darkselect <doniyor.kurbanov.24@gmail.com>
2025-06-12 14:32:50 +0500

    added forum-access-dto's

---
+++ src/main/java/uz/consortgroup/core/api/v1/dto/forum/ForumAccessResponse.java
@@ -0,0 +1,15 @@
+package uz.consortgroup.core.api.v1.dto.forum;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ForumAccessResponse {
+    private boolean hasAccess;
+    private ForumAccessReason reason;
+}
