922ed486dd85b7a5b9da39a0bdd7f28b4a301e67
Darkselect <doniyor.kurbanov.24@gmail.com>
2025-07-14 08:53:01 +0500

    added UserSearchRequestDto 1.0

--- src/main/java/uz/consortgroup/core/api/v1/dto/user/request/UserSearchRequest.java
+++ src/main/java/uz/consortgroup/core/api/v1/dto/user/request/UserSearchRequest.java
@@ -1,6 +1,7 @@
 package uz.consortgroup.core.api.v1.dto.user.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,5 +13,6 @@ import lombok.NoArgsConstructor;
 @Builder
 public class UserSearchRequest {
     @NotBlank(message = "Query is required")
+    @Size(min = 7, message = "Query must be at least 7 characters")
     private String query;
 }
