9eab81d5e65d7a041f7c0ba8481a1b0a30087c20
Darkselect <doniyor.kurbanov.24@gmail.com>
2025-07-14 10:26:35 +0500

    added UserSearchRequestDto 3.0

--- src/main/java/uz/consortgroup/core/api/v1/dto/user/response/UserSearchResponse.java
+++ src/main/java/uz/consortgroup/core/api/v1/dto/user/response/UserSearchResponse.java
@@ -6,11 +6,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import uz.consortgroup.core.api.v1.dto.user.enumeration.UserRole;
 
+import java.util.UUID;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
 public class UserSearchResponse {
+    private UUID userId;
     private String lastName;
     private String firstName;
     private String middleName;
