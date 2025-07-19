88b1a26edf2d8944152599dae6f3904f8dba12fd
Darkselect <doniyor.kurbanov.24@gmail.com>
2025-07-20 02:06:14 +0500

    added language to token

--- src/main/java/uz/consortgroup/core/api/v1/dto/user/request/RegisterDeviceTokenRequest.java
+++ src/main/java/uz/consortgroup/core/api/v1/dto/user/request/RegisterDeviceTokenRequest.java
@@ -1,11 +1,13 @@
 package uz.consortgroup.core.api.v1.dto.user.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import uz.consortgroup.core.api.v1.dto.user.enumeration.DeviceType;
+import uz.consortgroup.core.api.v1.dto.user.enumeration.Language;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -16,6 +18,8 @@ public class RegisterDeviceTokenRequest {
     private String fcmToken;
     @NotBlank(message = "Device type is required")
     private DeviceType deviceType;
+    @NotNull(message = "Language is required")
+    private Language language;
     private String appVersion;
     private String deviceInfo;
 }
