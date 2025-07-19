cd6bdf5ef3e03706bf04c4553ac192d398dff2c9
Darkselect <doniyor.kurbanov.24@gmail.com>
2025-07-19 21:39:50 +0500

    fcm token userid added

--- src/main/java/uz/consortgroup/core/api/v1/dto/user/response/FcmTokenDto.java
+++ src/main/java/uz/consortgroup/core/api/v1/dto/user/response/FcmTokenDto.java
@@ -6,11 +6,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import uz.consortgroup.core.api.v1.dto.user.enumeration.DeviceType;
 
+import java.util.UUID;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
 public class FcmTokenDto {
+    private UUID userId;
     private String fcmToken;
     private DeviceType deviceType;
 }
